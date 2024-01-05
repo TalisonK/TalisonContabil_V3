d0bdd59fd4b3e0542eccdab779d5352eaf981537
Talison Kennedy <talisonkennedy1@gmail.com>
2024-01-05 00:41:13 -0300

    correções para dockerização

--- contabil/src/main/java/br/com/talison/contabil/config/WebConfig.java
+++ contabil/src/main/java/br/com/talison/contabil/config/WebConfig.java
@@ -1,36 +1,14 @@
 package br.com.talison.contabil.config;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:4200",
-                                "http://localhost:3000",
-                                "http://localhost:80",
-                                "http://localhost:18000/",
-                                "http://front:80",
-                                "http://front",
-                                "http://localhost"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
-
 }
